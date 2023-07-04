dc310cbe8f8cd7b958aed2ad79d9c0b334a0d05c
pfuchs <patrick@42heilbronn.de>
2023-07-04 15:19:58 +0200

    fix: cors allow intra.42.fr

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -46,7 +46,7 @@ async fn main() -> Result<(), std::io::Error> {
         let client = awc::Client::default();
         let cors = Cors::default()
             .allowed_origin(&std::env::var("FRONTEND_URL").expect("env not set: FRONTEND_URL"))
-            .allowed_origin("https://*.intra.42.fr")
+            .allowed_origin("https://profile.intra.42.fr")
             .supports_credentials()
             .allow_any_header()
             .allow_any_method()
