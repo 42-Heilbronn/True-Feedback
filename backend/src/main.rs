2d69f942779a0533ce31aaf8371d460e21e354a6
pfuchs <patrick@42heilbronn.de>
2023-07-10 13:55:27 +0200

    allowed any origin

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -45,9 +45,10 @@ async fn main() -> Result<(), std::io::Error> {
         let auth_client = new_auth_client();
         let client = awc::Client::default();
         let cors = Cors::default()
-            .allowed_origin(&std::env::var("FRONTEND_URL").expect("env not set: FRONTEND_URL"))
-            .allowed_origin("https://profile.intra.42.fr")
-            .allowed_origin("https://api.intra.42.fr")
+            .allow_any_origin()
+            // .allowed_origin(&std::env::var("FRONTEND_URL").expect("env not set: FRONTEND_URL"))
+            // .allowed_origin("https://profile.intra.42.fr")
+            // .allowed_origin("https://api.intra.42.fr")
             .supports_credentials()
             .allow_any_header()
             .allow_any_method()
