e15efeba2a3cf6752766029faac3b0541b65573f
eschirni <joelschi@gmx.de>
2023-07-15 19:08:12 +0200

    added auth cookie to every request

--- frontend/playground.js
+++
@@ -1,6 +0,0 @@
-browser.runtime.onMessage.addListener((request, sender) => {
-    return new Promise((resolve, reject) => {
-        fetch(`https://dev01.playground.extension.42heilbronn.de/api/ping`)
-        .then(res => resolve(res.status));
-    });
-  });
